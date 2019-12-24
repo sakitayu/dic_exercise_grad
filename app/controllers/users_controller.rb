e71db5e2a82457724e19d2803d1a876ac53ceb6d
sakitayu <sakitakitakita@gmail.com>
2019-12-24 21:32:09 +0900

    新規登録ができないバグを修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,16 +14,9 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:id])
-    if @user.update(user_params)
-      redirect_to users_path
-    else
-      render :edit
-    end
   end
 
   def profile_update
-    
     if current_user.profile
       current_user.profile.update(profile_params)
     else
@@ -36,12 +29,8 @@ class UsersController < ApplicationController
 
   private
 
-  def user_params
-    params.require(:user).permit(:name, :age, :gender, :profile)
-  end
-
   def profile_params
-    params.require(:profile).permit(:name, :gender, :age, :introduction )
+    params.require(:profile).permit(:name, :gender, :age, :introduction)
   end
 
 end
