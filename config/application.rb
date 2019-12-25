0ec8430f29be632906c760fa62004a5fa6b3220d
sakitayu <sakitakitakita@gmail.com>
2019-12-25 16:39:33 +0900

    heroku用に微調整

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module Rainy
 
     config.assets.initialize_on_precompile = false
 
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
