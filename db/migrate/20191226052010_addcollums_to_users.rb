c5ac4c9cec84ddf4fc4821a1d31cc485b9630eb8
sakitayu <sakitakitakita@gmail.com>
2019-12-26 20:48:46 +0900

    usersテーブルの初期値を修正

--- db/migrate/20191226052010_addcollums_to_users.rb
+++ db/migrate/20191226052010_addcollums_to_users.rb
@@ -1,8 +1,8 @@
 class AddcollumsToUsers < ActiveRecord::Migration[5.2]
   def change
     add_column :users, :name, :string, null: false, default: "未設定"
-    add_column :users, :age, :integer, null: false, default: 3
-    add_column :users, :gender, :integer, null: false, default: 6
+    add_column :users, :age, :integer, null: false, default: 6
+    add_column :users, :gender, :integer, null: false, default: 3
     add_column :users, :introduction, :string
     add_column :users, :area, :string, null: false, default: "未入力"
     add_column :users, :have_umbrella, :boolean, default: false
