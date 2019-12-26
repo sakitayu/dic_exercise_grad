0b0c894f05e2106fa85c68a33759c6811a77acc0
sakitayu <sakitakitakita@gmail.com>
2019-12-26 20:13:51 +0900

    メッセージ画面に画面更新ボタンを実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,10 +13,10 @@ class User < ApplicationRecord
   has_one :profile, dependent: :destroy
 
   #validates :have_umbrella, presence: true
-  validates :area, presence: true
-  validates :name, presence: true
+  # validates :area, presence: true
+  # validates :name, presence: true
   #validates :gender, presence: true
-  validates :age, presence: true
+  # validates :age, presence: true
 
   enum gender: { "男": 1, "女": 2, "どちらでもない": 3 }
   enum age: { "20~26": 1, "27~33": 2, "34~40": 3, "41~47": 4, "48~60": 5, "非公開": 6 }
