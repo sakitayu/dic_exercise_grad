6b00bfd2cd0a030f8b129a6af9d607e43bf72c01
sakitayu <sakitakitakita@gmail.com>
2019-12-27 14:55:30 +0900

    サインアップ後にメールアドレス確認メールが届くことを画面上に表示する機能を追加

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  before_action :configure_sign_up_params, only: [:create]
+  # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
@@ -9,9 +9,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  POST /resource
+  # POST /resource
   def create
-    notice: "ブログを作成しました！"
     super
   end
 
@@ -56,8 +55,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super(resource)
   # end
 
+  private
   # The path used after sign up for inactive accounts.
-  # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def after_inactive_sign_up_path_for(resource)
+    new_user_session_path
+  end
 end
