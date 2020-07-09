7368a2272e7e68b81d61f2a384a18cae006930dc
sakitayu <sakitakitakita@gmail.com>
2020-07-09 23:14:09 +0900

    herokuでActionCableが有効になるようcable.ymlを修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,4 @@
 Rails.application.configure do
-  config.action_cable.allowed_request_origins = [ /https?:\/\/.*/ ]
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
