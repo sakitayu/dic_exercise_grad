9a2916c2d6e6dd64d50fb6cfb89a14d07b65c907
sakitayu <sakitakitakita@gmail.com>
2020-08-24 01:16:56 +0900

    傘なしユーザーがリクエスト確認画面からメッセージ画面に移動すると、傘持ちユーザー側のメッセージ画面にキャンセル通知が表示されるバグを修正

--- app/jobs/remove_broadcast_job.rb
+++ app/jobs/remove_broadcast_job.rb
@@ -4,7 +4,7 @@ class RemoveBroadcastJob < ApplicationJob
   def perform(ban)
     if ban.have_umbrella == true
       ActionCable.server.broadcast 'remove_channel', ban: ban, ban_id: ban.id, ban_removing: ban.removing
-    elsif ban.following.first.id != nil
+    elsif ban.following.first.id != nil && ban.state != "message"
       ActionCable.server.broadcast 'remove_channel', ban: render_cancel_notification(ban), ban_id: ban.id, ban_removing: ban.removing, ban_opposite_id: ban.following.first.id
     end
   end
