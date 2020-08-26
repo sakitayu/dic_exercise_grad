8041104cea9a00bf291c16557f559466003672cd
sakitayu <sakitakitakita@gmail.com>
2020-08-26 16:58:58 +0900

    相手からメッセージが届くと最下部の最新メッセージまでスクロールされる機能を実装

--- app/jobs/remove_broadcast_job.rb
+++ app/jobs/remove_broadcast_job.rb
@@ -4,6 +4,9 @@ class RemoveBroadcastJob < ApplicationJob
   def perform(ban)
     if ban.have_umbrella == true
       ActionCable.server.broadcast 'remove_channel', ban: ban, ban_id: ban.id, ban_removing: ban.removing
+      # 以下の条件分岐でメッセージ中はuserテーブルが更新されてもキャンセル通知が表示されないようにする
+      # 例えばメッセージ中にプロフィールを編集した時や
+      # 傘なしユーザーが「お願いする」を選択後にuserテーブルのstateカラムが"message"になった時
     elsif ban.following.first.id != nil && ban.state != "message"
       ActionCable.server.broadcast 'remove_channel', ban: render_cancel_notification(ban), ban_id: ban.id, ban_removing: ban.removing, ban_opposite_id: ban.following.first.id
     end
